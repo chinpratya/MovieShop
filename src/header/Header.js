5d5747f8e27ec6934988c71cac54b222396e3406
chinpratya <pratyaphromma@gmail.com>
2022-04-23 20:44:16 +0700

    update

--- src/header/Header.js
+++ src/header/Header.js
@@ -7,22 +7,22 @@ export default function Header() {
 
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark " >
+      <nav class="navbar navbar-expand-lg navbar-light " >
   <a className='brand' href="/">MovieShop</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+    <ul class="navbar-nav mr-auto ">
+      <li class="nav-item ">
         <a class="nav-link" href="/">หน้าหลัก</a>
       </li>
-      <li class="nav-item dropdown active">
+      <li class="nav-item dropdown ">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           ประเภท
         </a>
-        <div class="dropdown-menu active" aria-labelledby="navbarDropdown" >
+        <div class="dropdown-menu " aria-labelledby="navbarDropdown" >
           <Link to={`/category/10749/Romance`} class="dropdown-item" >Romance</Link>
           <div class="dropdown-divider"></div>
           <Link to={`/category/36/History`} class="dropdown-item" >History</Link>
@@ -38,14 +38,13 @@ export default function Header() {
           <Link to={`/category/18/Drama`} class="dropdown-item" >Drama</Link>
         </div>
       </li>
-      <li class="nav-item active">
+      <li class="nav-item ">
         <a class="nav-link " href="/shcart">ตะกร้าสินค้า</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input type="text" class="form-control mr-sm-2" placeholder="Search" aria-label="Search" onChange={e => setSearch(e.target.value)}/>
       <Link to={`/search/${search}`}><button class="btn btn-outline-success my-2 my-sm-0" type="submit" >Search</button></Link>
-      
     </form>
   </div>
 </nav>
